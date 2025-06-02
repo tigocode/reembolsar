83169f8adada5a0cf96ed15a8db42ecdbf131f84
tigocode <tiago.dantas16@live.com>
2025-06-01 23:21:43 -0300

    :sparkles: feat: Adiciona ícones e aprimora a interface da página de reembolsos com novos estilos e funcionalidades

--- src/app/Manager/Refunds/page.js
+++ src/app/Manager/Refunds/page.js
@@ -1,3 +1,7 @@
+import Lupa from '@/../../public/Icon.png'
+import Exit from '@/../../public/Frame.png'
+import Image from 'next/image'
+
 import styles from './Refunds.module.css';
 
 export default function PageRefunds() {
@@ -8,11 +12,62 @@ export default function PageRefunds() {
           <h1>Reembolsar</h1>
         </div>
         <div className={styles.containerUser}>
-          <h1>Tiago</h1>
+          <h1>Olá, Admin</h1>
+          <button>
+            <Image src={Exit} alt="Exit Icon" width={22} height={22} />
+          </button>
         </div>
       </div>
       <div className={styles.containerBox}>
-        <h1>TESTE</h1>
+        <div className={styles.containerTitle}>
+          <h2>Solicitações</h2>
+        </div>
+        <div className={styles.containerSeach}>
+          <input type="text" />
+          <button>
+            <Image src={Lupa} alt="Search Icon" className={styles.img} />
+          </button>
+        </div>
+        <div className={styles.containerTable}>
+          <table>
+            <thead>
+              <tr>
+                <th></th>
+                <th>Nome</th>
+                <th>Setor</th>
+                <th>Referência</th>
+                <th>Valor</th>
+                <th>Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <Image src={Exit} alt="Exit Icon" width={22} height={22} />
+                </td>
+                <td>João Silva</td>
+                <td>Engenharia</td>
+                <td>01/10/2023</td>
+               <td>R$ 150,00</td>
+                <td>
+                  <button>Visualizar</button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <Image src={Exit} alt="Exit Icon" width={22} height={22} />
+                </td>
+                <td>Maria Oliveira</td>
+                <td>O&M</td>
+                <td>02/10/2023</td>
+               <td>R$ 200,00</td>
+                <td>
+                  <button>Visualizar</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
